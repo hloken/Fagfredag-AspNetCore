df7794c084e608920a8fe945095d714a42ebf676
Hans Løken <hans@ebamse.net>
2016-10-22 14:22:25 +0200

    Did all kinds of crazy shit to get NUnit tests to run. Now works in VSTestRunner and commandline. Renamed some projects/directories.

---
+++ ForkusHotel/src/ForkusHotel.Api.IntegrationTests/BookingControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ForkusHotel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using Shouldly;
+
+namespace ForkusHotelApiIntegrationTests
+{
+    [TestFixture]
+    public class BookingControllerTests
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public BookingControllerTests()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Test]
+        public async Task HealthCheck()
+        {
+            var response = await _client.GetAsync("/api/booking/health");
+
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+    }
+}

d560f8cfc24573b4e3af5ef4e268bb3e0b965dc3
Hans Løken <hans@ebamse.net>
2016-10-22 13:18:09 +0200

    Added BookingController with HealthCheck method. Set default launch option to self-host at localhost:5000

---
+++ ForkusHotelApi/src/ForkusHotel/Controllers/BookingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForkusHotel.Controllers
+{
+    [Route("api/booking")]
+    public class BookingController : Controller
+    {
+        // GET api/booking
+        [HttpGet]
+        [Route("health")]
+        public bool HealthCheck()
+        {
+            return true;
+        }
+    }
+}

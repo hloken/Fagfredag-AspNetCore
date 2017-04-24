d9655ddc566010e885162f73a9b0562073aa985d
Hans Løken <hans@ebamse.net>
2017-04-24 09:55:12 +0200

    Upgraded solution and project files to VS2017 (csproj is back!). Updated all Nuget packages to latest (Asp.Net Core v1.1.1+)

--- ForkusHotel/src/ForkusHotel.Api.IntegrationTests/BookingControllerTests.cs
+++ ForkusHotel/src/ForkusHotel.Api.IntegrationTests/BookingControllerTests.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using ForkusHotel.Api/*.Solution*/;
+using ForkusHotel.Api.Solution;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Shouldly;
